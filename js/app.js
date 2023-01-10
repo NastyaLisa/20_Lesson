1456bee412d808b20d07e5ea6fbd02ba29200d24
NastyaLisa <lis.nastiona@gmail.com>
2023-01-11 01:08:16 +0200

    task 1-3

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// Task 1
+
+// function maxNumber(a, b) {
+//   if (a > b) {
+//         return a;
+ 
+//   }
+//   if (a < b) {
+//       return b;
+//   }
+//   else {
+//     return a = b;
+//   }
+
+// }
+// let a = prompt("first number", 'a');
+// let b = prompt("second number", 'b');
+
+// alert(maxNumber(a, b));
+
+// Task 2
+function revers(number) {
+  return number - (number * 2);
+};
+
+let number = prompt("add number", '');
+alert(revers(number));
+
+
+// //Task 3
+// function three(number, count) {
+//   return number + (3 * count);
+// }
+// let number = prompt("enter number", '');
+// let count = prompt("enter count", '');
+// alert(three(+number, +count));
+
+
